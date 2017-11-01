7e25025ce4f9a36a56052ecd34733a2aba5b71f7
elinalp <elina.lepetit@gmail.com>
2017-11-01 21:51:01 +0100

    Javadoc Generateur / generateurImpl

--- src/Metier/Generateur.java
+++ src/Metier/Generateur.java
@@ -1,8 +1,21 @@
 package Metier;
 
 /**
+ * L'interface Generateur est un observable, il implémente l'interface sujet du design pattern Observer
+ *
+ * @see Sujet
+ *
+ * @author Pierre-Louis Ollivier
+ * @author Elina Lepetit
+ * @version 1.0
  *
  */
 public interface Generateur extends Sujet {
-    String getValue(ObservateurGenerateurAsync oAfficheur);
+    /**
+     * Retourne la valeur sous la forme d'une chaine de caractère
+     * L'observateurGenerateur qui fait appel à getValue est passé en paramètre
+     * @param observateurGenerateur
+     * @return value
+     */
+    String getValue(ObservateurGenerateurAsync observateurGenerateur);
 }
--- src/Metier/GenerateurImpl.java
+++ src/Metier/GenerateurImpl.java
@@ -6,57 +6,127 @@ import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * La classe GenerateurImpl permet la génération de valeur.
+ * La génération choisie est celle de l'incrémentation de la valeur.
+ * GenerateurImpl implement Generateur et implicitement sujet, la classe est donc un observable.
+ *
+ *
+ * @see Sujet
+ * @see Generateur
+ *
+ * @author Pierre-Louis Ollivier
+ * @author Elina Lepetit
+ * @version 1.0
+ *
+ */
 public class GenerateurImpl implements Generateur {
 
+    /**
+     * L'algorithme de diffusion choisi pour la génération de valeur
+     */
     private AlgoDiffusion algo;
 
+    /**
+     * La valeur générée
+     */
     private Integer value;
 
+    /**
+     * Le statut du générateur.
+     * True le générateur est démarré, false le générateur est arrêté.
+     */
     private boolean statut;
 
+    /**
+     * Le scheduler du générateur
+     */
     private ScheduledExecutorService scheduler;
 
-    // Liste des canaux (Observeur) Observe la modification du générateur
+    /**
+     * La liste des canaux (Observateur) qui observent la modification du générateur
+     */
     private List<ObservateurGenerateurAsync> canauxObservers = new LinkedList<>();
 
+    /**
+     * Constructeur GenerateurImpl.
+     *
+     * @param scheduler
+     */
     public GenerateurImpl(ScheduledExecutorService scheduler){
         this.scheduler = scheduler;
     }
 
+    /**
+     * Met à jour l'algorithme choisi pour le générateur de valeur
+     * @param algo, le nouvel algorithme
+     */
     public void setAlgo(AlgoDiffusion algo) {
         this.algo = algo;
     }
 
+    /**
+     * @see Sujet#attach(ObservateurGenerateurAsync)
+     * @param observateur
+     */
     @Override
-    public void attach(ObservateurGenerateurAsync o) {
-        this.canauxObservers.add(o);
+    public void attach(ObservateurGenerateurAsync observateur) {
+        this.canauxObservers.add(observateur);
     }
 
+    /**
+     * @see Sujet#detach(ObservateurGenerateurAsync)
+     * @param observateur
+     */
     @Override
-    public void detach(ObservateurGenerateurAsync o) {
-        this.canauxObservers.remove(o);
+    public void detach(ObservateurGenerateurAsync observateur) {
+        this.canauxObservers.remove(observateur);
     }
 
-    // Retourne une copie de la liste des cannaux
-    public LinkedList<ObservateurGenerateurAsync> getCanauxObservers(){ return new LinkedList<>(this.canauxObservers); }
+    /**
+     * Retourne une copie de la liste des cannaux
+     * @return LinkedList de ObservateurGenerateurAsync
+     */
+    public LinkedList<ObservateurGenerateurAsync> getCanauxObservers() {
+        return new LinkedList<>(this.canauxObservers);
+    }
 
+    /**
+     * @see Generateur#getValue(ObservateurGenerateurAsync)
+     * @param oCanal
+     * @return la valeur du générateur traitée par l'algorithme
+     */
     @Override
     public String getValue(ObservateurGenerateurAsync oCanal) {
         return algo.getValue(oCanal);
     }
 
+    /**
+     * @return la valeur sous la forme d'une chaine de caractère.
+     */
     public String getValue(){
         return Integer.toString(this.value);
     }
 
-    // Simule le changement de valeur
+    /**
+     * La méthode setValue simule le changement de valeur du générateur.
+     * La génération choisie est celle de l'incrémentation de l'attribut value.
+     * A chaque changement de valeur on execute l'algorithme choisi.
+     * Condition : Pour que l'ensemble de ces instructions soient executées il faut que le générateur soit démarré.
+     */
     public void setValue(){
+        // Si le générateur est en fonctionnement
         if(statut){
             this.value++;
             algo.execute();
         }
     }
 
+    /**
+     * Lancement du générateur.
+     * L'algorithme défini est configuré,
+     * et la génération de valeur est lancée selon un temps défini en milliseconds via un scheduler.
+     */
     public void lancement(){
         this.statut = true;
         this.value = 0;
@@ -67,8 +137,16 @@ public class GenerateurImpl implements Generateur {
         }, 1000,1000, TimeUnit.MILLISECONDS);
     }
 
-    public void start() { this.statut = true; }
+    /**
+     * Démarre le générateur
+     */
+    public void start() {
+        this.statut = true;
+    }
 
+    /**
+     * Arrête le générateur
+     */
     public void stop(){
         this.statut = false;
     }
